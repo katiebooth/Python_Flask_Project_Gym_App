8e24665ea1a78c55ab7672b40463f66844b404dc
Katie Booth <katiebooth3@gmail.com>
2022-08-19 07:26:14 +0100

    First commit - created DB and added blank tables

---
+++ db/gym.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS classes;
+
+CREATE TABLE members (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  membership_type VARCHAR(255),
+  active BOOLEAN
+);
+
+CREATE TABLE classes (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  date DATE,
+  price INT,
+  capacity INT,
+  premium BOOLEAN
+);
+
+
+CREATE TABLE bookings(
+  id BIGSERIAL PRIMARY KEY,
+  member_id INT REFERENCES members(id) ON DELETE CASCADE,
+  classes_id INT REFERENCES classes(id) ON DELETE CASCADE
+);
